18599b0c7a6050f4cb02001d042fda12c0f078d5
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-04 14:19:34 +1000

    chore(params): remove redundant method rolled into tokens resolvers

--- resolvers/params.resolvers.ts
+++ resolvers/params.resolvers.ts
@@ -5,11 +5,6 @@ export default {
         sscstore: async (_: any) => {
             const results: any[] = await ssc.find('sscstore', 'params', {});
             return results;
-        },
-
-        tokens: async (_: any) => {
-            const results: any[] = await ssc.find('tokens', 'params', {});
-            return results;
         }
     }
 };
