7a28f46604a3d6de3d0acad4a221f33a50863cdf
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-04 14:19:22 +1000

    feat(market): resolvers with limit and offset values

--- resolvers/market.resolvers.ts
+++ resolvers/market.resolvers.ts
@@ -2,24 +2,24 @@ import { ssc } from '../client';
 
 export default {
     Query: {
-        buyBook: async (_, { symbol, account }) => {
+        buyBook: async (_, { symbol, account, limit = 200, offset = 0 }) => {
             const params: any = { symbol, account };
 
-            const results: any[] = await ssc.find('market', 'buyBook', params, 200, 0, [{ index: 'price', descending: true }], false);
+            const results: any[] = await ssc.find('market', 'buyBook', params, limit, offset, [{ index: 'price', descending: true }], false);
             return results;
         },
 
-        sellBook: async (_, { symbol, account }) => {
+        sellBook: async (_, { symbol, account, limit = 200, offset = 0 }) => {
             const params: any = { symbol, account };
 
-            const results: any[] = await ssc.find('market', 'sellBook', params, 200, 0, [{ index: 'price', descending: true }], false);
+            const results: any[] = await ssc.find('market', 'sellBook', params, limit, offset, [{ index: 'price', descending: true }], false);
             return results;
         },
 
-        tradesHistory: async (_, { symbol }) => {
+        tradesHistory: async (_, { symbol, limit = 30, offset = 0 }) => {
             const params: any = { symbol };
 
-            const results: any[] = await ssc.find('market', 'tradesHistory', params, 30, 0, [
+            const results: any[] = await ssc.find('market', 'tradesHistory', params, limit, offset, [
                 { 
                     index: 'timestamp', 
                     descending: false 
