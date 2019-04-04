faccff3b38bd1a3f48a41f569b01c8b769fb3e2f
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-04 16:19:25 +1000

    refactor(server): migrate to apollo server from yoga

--- index.ts
+++ index.ts
@@ -1,16 +1,22 @@
-import { GraphQLServer } from 'graphql-yoga';
+require('dotenv').config();
+
+import { ApolloServer } from 'apollo-server';
 import { default as typeDefs } from './typeDefs';
 import { default as resolvers } from './resolvers';
 
 const options = { port: process.env.PORT || 4999 };
 
-const server = new GraphQLServer({
-  typeDefs,
-  resolvers,
-});
+const server = new ApolloServer({
+  typeDefs: typeDefs as any,
+  resolvers: resolvers as any,
+  context: ({ req }) => ({
+    ...req
+  }),
+  playground: process.env.NODE_ENV === 'development',
+  debug: process.env.NODE_ENV === 'development'
+})
 
-server
-  .start(options, () =>
-    console.log(`Server is running ⚡ on localhost:${options.port}`),
-  )
-  .catch(err => console.error('connection Error', err));
+server.listen(options)
+  .then(({ url, server }) => {
+    console.log(`Server is running ⚡ on localhost:${options.port}`);
+  }).catch(err => console.error('connection Error', err));
