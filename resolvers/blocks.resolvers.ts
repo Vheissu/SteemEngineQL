22d85220a0e7890e2bafb582ceb4abcd71d20570
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-08 10:45:59 +1000

    feat(blocks): add in new resolvers and types for block info
    We needed to allow blocks (both latest and specific blocks by number) to be queried from the GraphQL node.

---
+++ resolvers/blocks.resolvers.ts
@@ -0,0 +1,40 @@
+import { ssc } from '../client';
+
+const parseBlockJsonStrings = result => {
+    if (result?.transactions) {
+        result.transactions = result.transactions.map(transaction => {
+            if (transaction?.payload) {
+                transaction.payload = JSON.parse(transaction.payload);
+            }
+        
+            if (transaction?.logs) {
+                transaction.logs = JSON.parse(transaction.logs);
+            }
+
+            return transaction;
+        });
+    }
+
+    return result;
+}
+
+export default {
+    Query: {
+        latestBlockInfo: async (_: any) => {
+            let result: any = await ssc.getLatestBlockInfo();
+
+            result = parseBlockJsonStrings(result);
+
+            return result;
+        },
+        blockInfo: async (_: any, {blockNumber}) => {
+            let result: any = await ssc.getBlockInfo(blockNumber);
+
+            console.log(result);
+
+            result = parseBlockJsonStrings(result);
+
+            return result;
+        }
+    }
+};
