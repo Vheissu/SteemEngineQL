d0580a4610bb77df426aedb50efa8cb052ef4728
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-09 23:59:21 +1000

    feat(steem): custom json mutation, fix resolvers

--- resolvers/steem.resolvers.ts
+++ resolvers/steem.resolvers.ts
@@ -1,13 +1,24 @@
 import * as steem from 'steem';
 
 export default {
-    Query: {
-        account: async (_: any, { name }) => {
-            let result: any = await steem.api.getAccountsAsync([name]);
+    Mutation: {
+        customJSON: async(_: any, { activeKey, name, id, json }) => {
+            const result = steem.broadcast.customJsonAsync(activeKey, [name], null, id, json);
 
             console.log(result);
 
             return result;
+        }
+    },
+    Query: {
+        account: async (_: any, { name }) => {
+            let result: any = await steem.api.getAccountsAsync([name]);
+
+            if (result[0]?.json_metadata) {
+                result[0].json_metadata = JSON.parse(result[0].json_metadata);
+            }
+
+            return result[0];
         },
         accounts: async (_: any, { names }) => {
             let result: any = await steem.api.getAccountsAsync(names);
