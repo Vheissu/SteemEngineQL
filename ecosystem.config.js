d70f621db5f77caf2b781a7a9388a4f16a66e330
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-18 12:13:57 +1000

    feat(pm2): add in staging mode for testnet

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,26 @@
 module.exports = {
-    apps : [{
-      name: "steem-engine-ql",
-      script: "./node_modules/.bin/ts-node",
-      args: "index.ts",
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      }
-    }]
-  }
+    apps: [
+        {
+            name: 'steem-engine-ql',
+            script: './node_modules/.bin/ts-node',
+            args: 'index.ts',
+            env: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
+        },
+        {
+            name: 'steem-engine-ql-testnet',
+            script: './node_modules/.bin/ts-node',
+            args: 'index.ts',
+            env: {
+                NODE_ENV: 'staging',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
+};
